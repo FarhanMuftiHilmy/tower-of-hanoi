d9997ab5ba622072f75e6dc7b5da6262f710f6a5
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-05-19 22:52:04 +0700

    Fixing error and documentation

--- hanoi_tower_oversimplified.cpp
+++ hanoi_tower_oversimplified.cpp
@@ -11,18 +11,17 @@ int main()
 	int jumlah_disk = 5;
 	
 	
-	//int A[jumlah_disk] = {1, 2, 3}; 	
 	int A[jumlah_disk] = {1, 2, 3, 4, 5};
 	int indeks_a = 0;
-	//int B[jumlah_disk] = {0, 0, 0};
 	int B[jumlah_disk] = {0, 0, 0, 0, 0};
 	int indeks_b = 0; 
-	//int C[jumlah_disk] = {0, 0, 0};
 	int C[jumlah_disk] = {0, 0, 0, 0, 0};
 	int indeks_c = 0;
-	int temp;
+	int temp = 0;
 	int loop = 0;
-	int apop, bpop, cpop;
+	int apop  = 0;
+	int bpop = 0;
+	int cpop = 0;
 	
 	
 	//Hitung banyak langkah
@@ -58,208 +57,6 @@ int main()
 		for(int i=0;i<jumlah_disk;i++){
 			if(A[i]!= 0){
 				indeks_a++;
-			}								
-		}
-		//count indeks c
-		for(int i=0;i<jumlah_disk;i++){
-			if(C[i]!= 0){
-				indeks_c++;
-			}				
-		}		
-		//PINDAH A KE C atau sebaliknya	
-			cpop = C[indeks_c-1];
-			apop = A[indeks_a-1];
-			if(apop>cpop){
-				temp = A[indeks_a-1];
-				A[indeks_a-1] = 0;
-				C[indeks_c] = temp;
-			} else{
-				temp = C[indeks_c-1];
-				C[indeks_c-1] = 0;				
-				A[indeks_a] = temp;			
-			}			
-		loop++;
-		cout << "langkah ke" << loop << endl;
-		//OUTPUT LANGKAH 1 dst
-		for(int i=0;i<jumlah_disk;i++){
-			cout << A[i] << endl;
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << B[i] << endl;;
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << C[i] << endl;
-		}
-		if(loop == n){
-			break;
-		}							
-		indeks_a = 0;	
-		indeks_c = 0;
-		//count indeks a
-		for(int i=0;i<jumlah_disk;i++){
-			if(A[i]!= 0){
-				indeks_a++;
-			} 	
-		}
-		//count indeks b
-		for(int i=0;i<jumlah_disk;i++){
-			if(B[i]!= 0){
-				indeks_b++;
-			} 
-		
-				
-					
-		}
-		//PINDAH A KE B atau sebaliknya
-		
-		apop = A[indeks_a-1];
-		bpop = B[indeks_b-1];
-		if(apop>bpop){
-			temp = A[indeks_a-1];
-			A[indeks_a-1] = 0;		
-			B[indeks_b] = temp;
-		} else{
-			temp = B[indeks_b-1];
-			B[indeks_b-1] = 0;
-			A[indeks_a] = temp;
-		}
-		loop++;
-		cout << "langkah ke" << loop << endl;
-		//OUTPUT LANGKAH 1 dst
-		for(int i=0;i<jumlah_disk;i++){
-			cout << A[i] << endl;
-			
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << B[i] << endl;;
-			
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << C[i] << endl;
-		}
-		if(loop == n){
-			break;
-		}		
-		
-		indeks_a = 0;
-		indeks_b = 0;
-		//count indeks b
-		for(int i=0;i<jumlah_disk;i++){
-			if(B[i]!= 0){
-				indeks_b++;
-			}					
-		}
-		//count indeks c
-		for(int i=0;i<jumlah_disk;i++){
-			if(C[i]!= 0){
-				indeks_c++;
-			}
-			
-		}
-		//PINDAH B KE C atau sebaliknya
-		
-		cpop = C[indeks_c-1];
-		bpop = B[indeks_b-1];
-		//PINDAH B KE C
-	 	if(bpop > cpop){
-	 	
-	 		temp = B[indeks_b-1];
-			B[indeks_b-1] = 0;
-			C[indeks_c] = temp;
-		 }else{ //PINDAH C KE B
-		 	temp = C[indeks_c-1];
-		 	C[indeks_c-1] = 0;
-		 	B[indeks_b] = temp;
-		 }
-		 loop++;
-		 cout << "langkah ke" << loop << endl;
-		//OUTPUT LANGKAH 1 dst
-		for(int i=0;i<jumlah_disk;i++){
-			cout << A[i] << endl;
-			
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << B[i] << endl;;
-			
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << C[i] << endl;
-		}
-		 if(loop == n){
-			break;
-		}		
-			
-		
-		indeks_b = 0;
-		indeks_c = 0;
-			
-			
-		}		
-		
-	
-	} else{ // BILANGAN GENAP
-		while(true){
-		
-		//count indeks a
-		for(int i=0;i<jumlah_disk;i++){
-			if(A[i]!= 0){
-				indeks_a++;
-			} 	
-		}
-		//count indeks b
-		for(int i=0;i<jumlah_disk;i++){
-			if(B[i]!= 0){
-				indeks_b++;
-			} 
-		
-				
-					
-		}
-		//PINDAH A KE B atau sebaliknya
-		
-		apop = A[indeks_a-1];
-		bpop = B[indeks_b-1];
-		if(apop>bpop){
-			temp = A[indeks_a-1];
-			A[indeks_a-1] = 0;		
-			B[indeks_b] = temp;
-		} else{
-			temp = B[indeks_b-1];
-			B[indeks_b-1] = 0;
-			A[indeks_a] = temp;
-		}
-		loop++;
-		cout << "langkah ke" << loop << endl;
-		//OUTPUT LANGKAH 1 dst
-		for(int i=0;i<jumlah_disk;i++){
-			cout << A[i] << endl;
-			
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << B[i] << endl;;
-			
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << C[i] << endl;
-		}
-		if(loop == n){
-			break;
-		}		
-		
-		indeks_a = 0;
-		indeks_b = 0;
-		//count indeks a
-		for(int i=0;i<jumlah_disk;i++){
-			if(A[i]!= 0){
-				indeks_a++;
 			}			
 					
 		}
@@ -274,29 +71,29 @@ int main()
 		
 		//PINDAH A KE C atau sebaliknya
 	
-			cpop = C[indeks_c-1];
-			apop = A[indeks_a-1];
-			if(apop>cpop){
-				temp = A[indeks_a-1];
-				A[indeks_a-1] = 0;
-				C[indeks_c] = temp;
+		if(indeks_a == 0){
+		    apop = A[indeks_a];
+		} else{
+		    apop = A[indeks_a-1];
+		}
+		if(indeks_c == 0){
+		    cpop = C[indeks_c];
+		} else{
+		    cpop = C[indeks_c-1];
+		}
+		
+		if(apop>cpop){
+			temp = A[indeks_a-1];
+			A[indeks_a-1] = 0;
+			C[indeks_c] = temp;		
+		} else{
+			temp = C[indeks_c-1];
+			C[indeks_c-1] = 0;
+			A[indeks_a] = temp;
+		}
+		
 						
-				
-				
-			
-			} else{
-				temp = C[indeks_c-1];
-				C[indeks_c-1] = 0;
-				
-					A[indeks_a] = temp;
-						
-			
-				
-			}				
-		
-					
-		
-		
+	
 	
 		loop++;
 		cout << "langkah ke" << loop << endl;
@@ -320,26 +117,91 @@ int main()
 			
 		indeks_a = 0;	
 		indeks_c = 0;
-		
+		//count indeks a
+		for(int i=0;i<jumlah_disk;i++){
+			if(A[i]!= 0){
+				indeks_a++;
+			} 	
+		}
 		//count indeks b
 		for(int i=0;i<jumlah_disk;i++){
 			if(B[i]!= 0){
 				indeks_b++;
-			}				
+			} 
+		
+		}
+		//PINDAH A KE B atau sebaliknya
+		if(indeks_a == 0){
+		    apop = A[indeks_a];
+		} else{
+		    apop = A[indeks_a-1];
+		}
+		if(indeks_b == 0){
+		    bpop = B[indeks_b];
+		} else{
+		    bpop = B[indeks_b-1];
+		}
+		
+
+		if(apop>bpop){
+			temp = A[indeks_a-1];
+			A[indeks_a-1] = 0;		
+			B[indeks_b] = temp;
+		} else{
+			temp = B[indeks_b-1];
+			B[indeks_b-1] = 0;
+			A[indeks_a] = temp;
+		}
+		loop++;
+	
+		cout << "langkah ke" << loop << endl;
+		//OUTPUT LANGKAH 1 dst
+		for(int i=0;i<jumlah_disk;i++){
+			cout << A[i] << endl;
+			
+		}
+		cout << "-----" << endl;
+		for(int i=0;i<jumlah_disk;i++){
+			cout << B[i] << endl;;
+			
+		}
+		cout << "-----" << endl;
+		for(int i=0;i<jumlah_disk;i++){
+			cout << C[i] << endl;
+		}
+		if(loop == n){
+			break;
+		}		
+	
+		indeks_a = 0;
+		indeks_b = 0;
+		//count indeks b
+		for(int i=0;i<jumlah_disk;i++){
+			if(B[i]!= 0){
+				indeks_b++;
+			}			
 		}
 		//count indeks c
 		for(int i=0;i<jumlah_disk;i++){
 			if(C[i]!= 0){
 				indeks_c++;
-			}	
+			}
 		}
 		//PINDAH B KE C atau sebaliknya
+        if(indeks_c == 0){
+		    cpop = C[indeks_c];
+		} else{
+		    cpop = C[indeks_c-1];
+		}
+		if(indeks_b == 0){
+		    bpop = B[indeks_b];
+		} else{
+		    bpop = B[indeks_b-1];
+		}
+		    
 		
-		cpop = C[indeks_c-1];	
-		bpop = B[indeks_b-1];
 		//PINDAH B KE C
 	 	if(bpop > cpop){
-	 	
 	 		temp = B[indeks_b-1];
 			B[indeks_b-1] = 0;
 			C[indeks_c] = temp;
@@ -368,12 +230,13 @@ int main()
 			break;
 		}		
 			
-		
+	
 		indeks_b = 0;
 		indeks_c = 0;
 			
 			
 		}
+
 	}
 	
 	
