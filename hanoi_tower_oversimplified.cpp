046a38c40e816856d1f2d32588966e8d228c1f04
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-05-20 11:23:40 +0700

    Add implementation for even numbers

--- hanoi_tower_oversimplified.cpp
+++ hanoi_tower_oversimplified.cpp
@@ -52,184 +52,364 @@ int main()
 	
 	if(jumlah_disk%2 != 0){
 		while(true){
-			
-		//count indeks a
-		for(int i=0;i<jumlah_disk;i++){
-			if(A[i]!= 0){
-				indeks_a++;
-			}			
-					
-		}
-		//count indeks c
-		for(int i=0;i<jumlah_disk;i++){
-			if(C[i]!= 0){
-				indeks_c++;
-			}	
-			
-		}
-		
-		
-		//PINDAH A KE C atau sebaliknya
-	
-		if(indeks_a == 0){
-		    apop = A[indeks_a];
-		} else{
-		    apop = A[indeks_a-1];
-		}
-		if(indeks_c == 0){
-		    cpop = C[indeks_c];
-		} else{
-		    cpop = C[indeks_c-1];
-		}
-		
-		if(apop>cpop){
-			temp = A[indeks_a-1];
-			A[indeks_a-1] = 0;
-			C[indeks_c] = temp;		
-		} else{
-			temp = C[indeks_c-1];
-			C[indeks_c-1] = 0;
-			A[indeks_a] = temp;
-		}
-		loop++;
-		
-		cout << "langkah ke" << loop << endl;
-		//OUTPUT LANGKAH 1 dst
-		for(int i=0;i<jumlah_disk;i++){
-			cout << A[i] << endl;
-			
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << B[i] << endl;;
-			
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << C[i] << endl;
-		}
-		if(loop == n){
-			break;
-		}					
-			
-		indeks_a = 0;	
-		indeks_c = 0;
-		//count indeks a
-		for(int i=0;i<jumlah_disk;i++){
-			if(A[i]!= 0){
-				indeks_a++;
-			} 	
-		}
-		//count indeks b
-		for(int i=0;i<jumlah_disk;i++){
-			if(B[i]!= 0){
-				indeks_b++;
-			} 
-		
-		}
-		//PINDAH A KE B atau sebaliknya
-		if(indeks_a == 0){
-		    apop = A[indeks_a];
-		} else{
-		    apop = A[indeks_a-1];
-		}
-		if(indeks_b == 0){
-		    bpop = B[indeks_b];
-		} else{
-		    bpop = B[indeks_b-1];
-		}
-		
-
-		if(apop>bpop){
-			temp = A[indeks_a-1];
-			A[indeks_a-1] = 0;		
-			B[indeks_b] = temp;
-		} else{
-			temp = B[indeks_b-1];
-			B[indeks_b-1] = 0;
-			A[indeks_a] = temp;
-		}
-		loop++;
-	
-		cout << "langkah ke" << loop << endl;
-		//OUTPUT LANGKAH 1 dst
-		for(int i=0;i<jumlah_disk;i++){
-			cout << A[i] << endl;
-			
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << B[i] << endl;;
-			
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << C[i] << endl;
-		}
-		if(loop == n){
-			break;
-		}		
-	
-		indeks_a = 0;
-		indeks_b = 0;
-		//count indeks b
-		for(int i=0;i<jumlah_disk;i++){
-			if(B[i]!= 0){
-				indeks_b++;
-			}			
-		}
-		//count indeks c
-		for(int i=0;i<jumlah_disk;i++){
-			if(C[i]!= 0){
-				indeks_c++;
+		if(jumlah_disk%2 != 0){
+			//count indeks a
+			for(int i=0;i<jumlah_disk;i++){
+				if(A[i]!= 0){
+					indeks_a++;
+				}			
+						
 			}
-		}
-		//PINDAH B KE C atau sebaliknya
-        if(indeks_c == 0){
-		    cpop = C[indeks_c];
-		} else{
-		    cpop = C[indeks_c-1];
-		}
-		if(indeks_b == 0){
-		    bpop = B[indeks_b];
-		} else{
-		    bpop = B[indeks_b-1];
-		}
-		    
+			//count indeks c
+			for(int i=0;i<jumlah_disk;i++){
+				if(C[i]!= 0){
+					indeks_c++;
+				}	
+				
+			}
+			
+			
+			//PINDAH A KE C atau sebaliknya
 		
-		//PINDAH B KE C
-	 	if(bpop > cpop){
-	 		temp = B[indeks_b-1];
-			B[indeks_b-1] = 0;
-			C[indeks_c] = temp;
-		 }else{ //PINDAH C KE B
-		 	temp = C[indeks_c-1];
-		 	C[indeks_c-1] = 0;
-		 	B[indeks_b] = temp;
-		 }
-		 loop++;
-		 cout << "langkah ke" << loop << endl;
-		//OUTPUT LANGKAH 1 dst
-		for(int i=0;i<jumlah_disk;i++){
-			cout << A[i] << endl;
+			if(indeks_a == 0){
+			    apop = A[indeks_a];
+			} else{
+			    apop = A[indeks_a-1];
+			}
+			if(indeks_c == 0){
+			    cpop = C[indeks_c];
+			} else{
+			    cpop = C[indeks_c-1];
+			}
 			
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << B[i] << endl;;
+			if(apop>cpop){
+				temp = A[indeks_a-1];
+				A[indeks_a-1] = 0;
+				C[indeks_c] = temp;		
+			} else{
+				temp = C[indeks_c-1];
+				C[indeks_c-1] = 0;
+				A[indeks_a] = temp;
+			}
+			loop++;
 			
-		}
-		cout << "-----" << endl;
-		for(int i=0;i<jumlah_disk;i++){
-			cout << C[i] << endl;
-		}
-		 if(loop == n){
-			break;
-		}		
+			cout << "langkah ke" << loop << endl;
+			//OUTPUT LANGKAH 1 dst
+			for(int i=0;i<jumlah_disk;i++){
+				cout << A[i] << endl;
+				
+			}
+			cout << "-----" << endl;
+			for(int i=0;i<jumlah_disk;i++){
+				cout << B[i] << endl;;
+				
+			}
+			cout << "-----" << endl;
+			for(int i=0;i<jumlah_disk;i++){
+				cout << C[i] << endl;
+			}
+			if(loop == n){
+				break;
+			}					
+				
+			indeks_a = 0;	
+			indeks_c = 0;
+			//count indeks a
+			for(int i=0;i<jumlah_disk;i++){
+				if(A[i]!= 0){
+					indeks_a++;
+				} 	
+			}
+			//count indeks b
+			for(int i=0;i<jumlah_disk;i++){
+				if(B[i]!= 0){
+					indeks_b++;
+				} 
+			
+			}
+			//PINDAH A KE B atau sebaliknya
+			if(indeks_a == 0){
+			    apop = A[indeks_a];
+			} else{
+			    apop = A[indeks_a-1];
+			}
+			if(indeks_b == 0){
+			    bpop = B[indeks_b];
+			} else{
+			    bpop = B[indeks_b-1];
+			}
 			
 	
-		indeks_b = 0;
-		indeks_c = 0;
+			if(apop>bpop){
+				temp = A[indeks_a-1];
+				A[indeks_a-1] = 0;		
+				B[indeks_b] = temp;
+			} else{
+				temp = B[indeks_b-1];
+				B[indeks_b-1] = 0;
+				A[indeks_a] = temp;
+			}
+			loop++;
+		
+			cout << "langkah ke" << loop << endl;
+			//OUTPUT LANGKAH 1 dst
+			for(int i=0;i<jumlah_disk;i++){
+				cout << A[i] << endl;
+				
+			}
+			cout << "-----" << endl;
+			for(int i=0;i<jumlah_disk;i++){
+				cout << B[i] << endl;;
+				
+			}
+			cout << "-----" << endl;
+			for(int i=0;i<jumlah_disk;i++){
+				cout << C[i] << endl;
+			}
+			if(loop == n){
+				break;
+			}		
+		
+			indeks_a = 0;
+			indeks_b = 0;
+			//count indeks b
+			for(int i=0;i<jumlah_disk;i++){
+				if(B[i]!= 0){
+					indeks_b++;
+				}			
+			}
+			//count indeks c
+			for(int i=0;i<jumlah_disk;i++){
+				if(C[i]!= 0){
+					indeks_c++;
+				}
+			}
+			//PINDAH B KE C atau sebaliknya
+	        if(indeks_c == 0){
+			    cpop = C[indeks_c];
+			} else{
+			    cpop = C[indeks_c-1];
+			}
+			if(indeks_b == 0){
+			    bpop = B[indeks_b];
+			} else{
+			    bpop = B[indeks_b-1];
+			}
+			    
+			
+			//PINDAH B KE C
+		 	if(bpop > cpop){
+		 		temp = B[indeks_b-1];
+				B[indeks_b-1] = 0;
+				C[indeks_c] = temp;
+			 }else{ //PINDAH C KE B
+			 	temp = C[indeks_c-1];
+			 	C[indeks_c-1] = 0;
+			 	B[indeks_b] = temp;
+			 }
+			 loop++;
+			 cout << "langkah ke" << loop << endl;
+			//OUTPUT LANGKAH 1 dst
+			for(int i=0;i<jumlah_disk;i++){
+				cout << A[i] << endl;
+				
+			}
+			cout << "-----" << endl;
+			for(int i=0;i<jumlah_disk;i++){
+				cout << B[i] << endl;;
+				
+			}
+			cout << "-----" << endl;
+			for(int i=0;i<jumlah_disk;i++){
+				cout << C[i] << endl;
+			}
+			 if(loop == n){
+				break;
+			}		
+				
+		
+			indeks_b = 0;
+			indeks_c = 0;
+		} else{			
+			//count indeks a
+			for(int i=0;i<jumlah_disk;i++){
+				if(A[i]!= 0){
+					indeks_a++;
+				} 	
+			}
+			//count indeks b
+			for(int i=0;i<jumlah_disk;i++){
+				if(B[i]!= 0){
+					indeks_b++;
+				} 
+			
+			}
+			//PINDAH A KE B atau sebaliknya
+			if(indeks_a == 0){
+			    apop = A[indeks_a];
+			} else{
+			    apop = A[indeks_a-1];
+			}
+			if(indeks_b == 0){
+			    bpop = B[indeks_b];
+			} else{
+			    bpop = B[indeks_b-1];
+			}
+			
+	
+			if(apop>bpop){
+				temp = A[indeks_a-1];
+				A[indeks_a-1] = 0;		
+				B[indeks_b] = temp;
+			} else{
+				temp = B[indeks_b-1];
+				B[indeks_b-1] = 0;
+				A[indeks_a] = temp;
+			}
+			loop++;
+		
+			cout << "langkah ke" << loop << endl;
+			//OUTPUT LANGKAH 1 dst
+			for(int i=0;i<jumlah_disk;i++){
+				cout << A[i] << endl;
+				
+			}
+			cout << "-----" << endl;
+			for(int i=0;i<jumlah_disk;i++){
+				cout << B[i] << endl;;
+				
+			}
+			cout << "-----" << endl;
+			for(int i=0;i<jumlah_disk;i++){
+				cout << C[i] << endl;
+			}
+			if(loop == n){
+				break;
+			}		
+		
+			indeks_a = 0;
+			indeks_b = 0;
+			//count indeks a
+			for(int i=0;i<jumlah_disk;i++){
+				if(A[i]!= 0){
+					indeks_a++;
+				}			
+						
+			}
+			//count indeks c
+			for(int i=0;i<jumlah_disk;i++){
+				if(C[i]!= 0){
+					indeks_c++;
+				}	
+				
+			}
+			
+			
+			//PINDAH A KE C atau sebaliknya
+		
+			if(indeks_a == 0){
+			    apop = A[indeks_a];
+			} else{
+			    apop = A[indeks_a-1];
+			}
+			if(indeks_c == 0){
+			    cpop = C[indeks_c];
+			} else{
+			    cpop = C[indeks_c-1];
+			}
+			
+			if(apop>cpop){
+				temp = A[indeks_a-1];
+				A[indeks_a-1] = 0;
+				C[indeks_c] = temp;		
+			} else{
+				temp = C[indeks_c-1];
+				C[indeks_c-1] = 0;
+				A[indeks_a] = temp;
+			}
+			loop++;
+			
+			cout << "langkah ke" << loop << endl;
+			//OUTPUT LANGKAH 1 dst
+			for(int i=0;i<jumlah_disk;i++){
+				cout << A[i] << endl;
+				
+			}
+			cout << "-----" << endl;
+			for(int i=0;i<jumlah_disk;i++){
+				cout << B[i] << endl;;
+				
+			}
+			cout << "-----" << endl;
+			for(int i=0;i<jumlah_disk;i++){
+				cout << C[i] << endl;
+			}
+			if(loop == n){
+				break;
+			}					
+				
+			indeks_a = 0;	
+			indeks_c = 0;
+			//count indeks b
+			for(int i=0;i<jumlah_disk;i++){
+				if(B[i]!= 0){
+					indeks_b++;
+				}			
+			}
+			//count indeks c
+			for(int i=0;i<jumlah_disk;i++){
+				if(C[i]!= 0){
+					indeks_c++;
+				}
+			}
+			//PINDAH B KE C atau sebaliknya
+	        if(indeks_c == 0){
+			    cpop = C[indeks_c];
+			} else{
+			    cpop = C[indeks_c-1];
+			}
+			if(indeks_b == 0){
+			    bpop = B[indeks_b];
+			} else{
+			    bpop = B[indeks_b-1];
+			}
+			    
+			
+			//PINDAH B KE C
+		 	if(bpop > cpop){
+		 		temp = B[indeks_b-1];
+				B[indeks_b-1] = 0;
+				C[indeks_c] = temp;
+			 }else{ //PINDAH C KE B
+			 	temp = C[indeks_c-1];
+			 	C[indeks_c-1] = 0;
+			 	B[indeks_b] = temp;
+			 }
+			 loop++;
+			 cout << "langkah ke" << loop << endl;
+			//OUTPUT LANGKAH 1 dst
+			for(int i=0;i<jumlah_disk;i++){
+				cout << A[i] << endl;
+				
+			}
+			cout << "-----" << endl;
+			for(int i=0;i<jumlah_disk;i++){
+				cout << B[i] << endl;;
+				
+			}
+			cout << "-----" << endl;
+			for(int i=0;i<jumlah_disk;i++){
+				cout << C[i] << endl;
+			}
+			 if(loop == n){
+				break;
+			}		
+				
+		
+			indeks_b = 0;
+			indeks_c = 0;
+		}
+		
 			
 			
 		}
